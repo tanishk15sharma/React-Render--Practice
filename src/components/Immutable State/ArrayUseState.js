db4b69ad03b30d1509170650f8ac67b390746791
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-20 16:04:16 +0530

    new Array

--- src/components/Immutable State/ArrayUseState.js
+++ src/components/Immutable State/ArrayUseState.js
@@ -3,22 +3,32 @@ import React, { useState } from "react";
 const initialState = ["Tanishk", " sharma"];
 
 const ArrayUseStateFunction = () => {
-  const [person, setPerson] = useState(initialState);
+  const [persons, setPerson] = useState(initialState);
+
+  //   const HandleName = () => {
+  //     //this will not ADD the names in DOM because when we push elemt to same array the array value cahnges but array reference itself does not change ..therefore we need to make a NEW array
+  //     person.push("javascript");
+  //     person.push("react");
+
+  //     setPerson(person);
+  //   };
 
   const HandleName = () => {
-    //this will not ADD the names in DOM because when we push elemt to same array the array value cahnges but array reference itself does not change ..therefore we need to make a NEW array
-    person.push("javascript");
-    person.push("react");
+    //Now we r good to go , as new arry will give the refeence too
+    //therefor make a new copy of OBJECT or ARRAY ,modify it and pass it
+    const NewArrayPerson = [...persons];
+    NewArrayPerson.push("web-developer");
+    NewArrayPerson.push("Coder");
 
-    setPerson(person);
+    setPerson(NewArrayPerson);
   };
 
   console.log("Immutable UseState Render");
   return (
     <div>
       <button onClick={HandleName}>Click</button>
-      {person.map((name) => (
-        <div key={name}> {name} </div>
+      {persons.map((P) => (
+        <div key={P}> {P} </div>
       ))}
     </div>
   );
