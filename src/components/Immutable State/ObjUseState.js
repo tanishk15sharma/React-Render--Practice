4e49a37ade7ab8826598c344b55ad773a8b771ae
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-20 15:38:49 +0530

    newObj -UseState

--- src/components/Immutable State/ObjUseState.js
+++ src/components/Immutable State/ObjUseState.js
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
 
 const initialObj = {
-  fname: "tanishk",
-  lname: "sharma",
+  fname: "Tanishk",
+  lname: "Sharma",
 };
 
-console.log(initialObj);
+// console.log(initialObj);
 
 const ObjectUseState = () => {
   const [person, setPerson] = useState(initialObj);
 
+  //   const changeName = () => {                This will not work , beacuse directly mutating property of Obj or array will not change the values of the Obj
+  //     person.fname = "Webdeveloper";
+  //     person.lname = "Tanishk";
+
+  //     setPerson(person);
+  //   };
+
+  //so we have to do it like this
+
+  const changeName = () => {
+    //now it's good to go
+    const NewPersonObj = { ...initialObj };
+    NewPersonObj.fname = "Web-DEV";
+    NewPersonObj.lname = "Tanishk";
+
+    setPerson(NewPersonObj);
+  };
+
+  console.log("Immutable UseState  Render");
+
   return (
     <div>
-      <button>
+      <button onClick={changeName}>
         {person.fname} {person.lname}
       </button>
     </div>
